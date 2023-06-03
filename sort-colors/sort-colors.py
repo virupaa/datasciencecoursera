cf2d11b23721b0acc10a6ea5826160f4592fceac
Chinmay <43137090+virupaa@users.noreply.github.com>
2023-06-03 17:03:53 -0400

    Time: 45 ms, Memory: 16.1 MB - LeetHub

--- sort-colors/sort-colors.py
+++ sort-colors/sort-colors.py
@@ -1,11 +1,7 @@
-from collections import OrderedDict
-from itertools import repeat
- 
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-       
-        nums[:] = [0] * nums.count(0) + [1] * nums.count(1) + [2] * nums.count(2)
+        nums[:]= [0]*nums.count(0)+[1]*nums.count(1)+[2]*nums.count(2)
         
